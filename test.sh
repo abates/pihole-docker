2031cacf96fcb556db2a470873dec3ffeb2175a8
Andrew Bates <abates@omeganetserv.com>
2021-01-06 08:56:57 -0500

    Fixed example usage

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -p 80:80 -p 443:443 --volume=$(pwd)/test.d:/etc/upstream.d --volume=$(pwd)/test:/etc/upstream abates314/pihole
+docker run --rm -p 80:80 -p 443:443 --volume=$(pwd)/test.d:/etc/upstream.d --volume=$(pwd)/test/static_hosts:/etc/static_hosts abates314/pihole
 
